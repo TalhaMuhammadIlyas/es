58dc8abf028d8d9095a98e71332b912bf7d70ff0
Talha Ilyas <talhailyas1998@gmail.com>
2025-08-14 21:16:58 +0500

    refactor: update locators in BookSession, GiftWallet, and SignupScreen for consistency and clarity

--- test/screenobjects/gift_wallet-locators.ts
+++ test/screenobjects/gift_wallet-locators.ts
@@ -1,14 +1,9 @@
-import { $ } from "@wdio/globals";
-console.log('Loaded gift_wallet-locators');
 class GiftWalletLocators {
     get bottom_nav_menu() {
         return $('~Menu');
     }
     get gift_wallet_menu() {
         return $('//android.view.ViewGroup[@content-desc="menu_btn_top_up_friends_wallet"]/android.view.ViewGroup');
-    }
-    get gift_wallet_input_phone() {
-        return $('~input_phone');
     }
     get gift_wallet_email_switcher() {
         return $('~login_switcher_email_option');
@@ -17,13 +12,10 @@ class GiftWalletLocators {
         return $('~gift_friend_contact_details_button_next');
     }
     get gift_wallet_select_preset() {
-        return $('//android.view.ViewGroup[@content-desc="200"]/android.view.ViewGroup');
-    }
-    get gift_wallet_input_amount() {
-        return $('~gift_amount_input');
+        return $('~gift_wallet_preset_amount');
     }
     get gift_wallet_next_button() {
-        return $('~gift_friend_wallet_btn_next');
+        return $('~gift_wallet_next_btn');
     }
     get gift_wallet_input_name() {
         return $('~gift_wallet_name_input');
@@ -32,10 +24,10 @@ class GiftWalletLocators {
         return $('~gift_wallet_message_input');
     }
     get gift_wallet_next2_button() {
-        return $('~gift_wallet_next_button');
+        return $('~gift_wallet_next2_btn');
     }
     get add_new_card_button() {
-        return $('//android.widget.TextView[@text="New card"]');
+        return $('~add_new_card_btn');
     }
 }
 
--- test/screenobjects/signup_screen-locators.ts
+++ test/screenobjects/signup_screen-locators.ts
@@ -1,5 +1,3 @@
-import { $ } from "@wdio/globals";
-console.log('Loaded book_session-locators');
 class SignupScreenLocators {
     get nextbutton() {
         return $('~app_tour_footer_btn_next');
@@ -11,64 +9,52 @@ class SignupScreenLocators {
         return $('~Menu');
     }
     get guestmenu_loginbtn() {
-        return $('~menu_btn_login');
+        return $('~guest_menu_login_btn');
     }
     get emailswitcher() {
-        return $('//android.view.ViewGroup[@resource-id="login_switcher"]/android.view.ViewGroup/android.view.ViewGroup/android.view.ViewGroup');
+        return $('~login_switcher_email_option');
     }
     get login_inputemail() {
         return $('~login_input_email');
     }
     get login_nextbtn() {
-        return $('~login_btn_next');
+        return $('~login_next_btn');
     }
     get otp() {
-        return $('//android.widget.EditText');
+        return $('~otp_input');
     }
     get otp_nextbtn() {
-        return $('~verification_btn_next');
+        return $('~otp_next_btn');
     }
     get input_name() {
-        return $('~signup_details_input_name');
+        return $('~signup_input_name');
     }
     get inputname_nextbtn() {
-        return $('~signup_details_btn_next');
+        return $('~signup_name_next_btn');
     }
     get gender_bottomsheet() {
-        return $('~signup_details_bottom_sheet_gender');
+        return $('~gender_bottomsheet');
     }
     get male_optionselect() {
-        return $('(//android.view.ViewGroup[@content-desc="bottom-sheet-option-[object Object]"])[1]');
+        return $('~male_option');
     }
     get gender_nextbtn() {
-        return $('~signup_details_btn_next');
-    }
-    get date_picker() {
-        return $('~signup_details_date_picker_date_of_birth');
-    }
-    get month_datepick() {
-        return $('//android.widget.NumberPicker[1]');
-    }
-    get day_datepick() {
-        return $('//android.widget.NumberPicker[2]');
-    }
-    get year_datepick() {
-        return $('//android.widget.NumberPicker[3]');
+        return $('~gender_next_btn');
     }
     get password() {
-        return $('~signup_details_input_password');
+        return $('~password_input');
     }
     get confirm_password() {
-        return $('~signup_details_input_confirm_password');
+        return $('~confirm_password_input');
     }
     get password_nextbtn() {
-        return $('~signup_details_btn_next');
+        return $('~password_next_btn');
     }
     get welcome_message() {
-        return $('//android.widget.TextView[@text="Glad to have you at Estenarh!"]');
+        return $('~welcome_message');
     }
     get welcome_nextbtn() {
-        return $('~welcome_btn_start');
+        return $('~welcome_next_btn');
     }
 }
 
