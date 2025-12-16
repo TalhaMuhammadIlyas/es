2f95f87d636d211d86a401e19bf73cd9d78d0a01
Talha Muhammad Ilyas <talhailyas1998@gmail.com>
2025-12-16 14:23:19 +0500

    feat: add miscellaneous constants for consultant and client details

---
+++ test/constants/miscellaneous.ts
@@ -0,0 +1,10 @@
+export const miscellaneous = {
+    ConsultantName: 'Rohan Ul Haq',
+    ConsultantName2: 'Nawaz Sharif',
+    ConsultantName3: 'Ahmed Ali',
+    ConsultantName4: 'Nayela Shafi',
+    ClientMail: 'rohanulhaq004@gmail.com',
+    ClientOTP: '1234',
+    ClientPassword: 'click123',
+    ClientConfirmPassword: 'click123'
+} as const;
